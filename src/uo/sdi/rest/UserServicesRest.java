976ac64c2de565159db8b69a426f53fd2b2acef2
Fuegon <fuegon.contacto@gmail.com>
2017-04-25 20:09:11 +0200

    Adapatado REST a autorizacion

--- src/uo/sdi/rest/UserServicesRest.java
+++ src/uo/sdi/rest/UserServicesRest.java
@@ -2,6 +2,7 @@ package uo.sdi.rest;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import uo.sdi.business.exception.BusinessException;
@@ -20,8 +22,8 @@ public interface UserServicesRest {
 
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	@Path("userCats/{userid}")
-	List<Category> listarCategorias(@PathParam("userid") Long userId)
+	@Path("userCats")
+	List<Category> listarCategorias(@Context HttpServletRequest req)
 			throws BusinessException;
 
 	@GET
@@ -36,6 +38,6 @@ public interface UserServicesRest {
 
 	@POST
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	void saveTarea(Task tarea) throws BusinessException;
+	void saveTarea(Task tarea, @Context HttpServletRequest req) throws BusinessException;
 
 }
--- src/uo/sdi/rest/UserServicesRestImpl.java
+++ src/uo/sdi/rest/UserServicesRestImpl.java
@@ -2,11 +2,14 @@ package uo.sdi.rest;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import alb.util.date.DateUtil;
 import uo.sdi.business.TaskService;
 import uo.sdi.business.exception.BusinessException;
 import uo.sdi.dto.Category;
 import uo.sdi.dto.Task;
+import uo.sdi.dto.User;
 import uo.sdi.infrastructure.Factories;
 
 public class UserServicesRestImpl implements UserServicesRest {
@@ -14,9 +17,10 @@ public class UserServicesRestImpl implements UserServicesRest {
 	private TaskService taskService = Factories.services.getTaskService();
 
 	@Override
-	public List<Category> listarCategorias(Long userId)
+	public List<Category> listarCategorias(HttpServletRequest req)
 			throws BusinessException {
-		return taskService.findCategoriesByUserId(userId);
+		User u = (User) req.getSession(true).getAttribute("user");
+		return taskService.findCategoriesByUserId(u.getId());
 	}
 
 	@Override
@@ -26,12 +30,15 @@ public class UserServicesRestImpl implements UserServicesRest {
 
 	@Override
 	public void finalizarTarea(Task tarea) throws BusinessException {
-		tarea.setFinished(DateUtil.today());
-		taskService.updateTask(tarea);
+		Task task = taskService.findTaskById(tarea.getId());
+		task.setFinished(DateUtil.today());
+		taskService.updateTask(task);
 	}
 
 	@Override
-	public void saveTarea(Task tarea) throws BusinessException {
+	public void saveTarea(Task tarea, HttpServletRequest req) throws BusinessException {
+		User u = (User) req.getSession(true).getAttribute("user");
+		tarea.setUserId(u.getId());
 		taskService.createTask(tarea);
 	}
 
